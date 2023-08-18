bca50c2b07b6614105c03882493347912f9b60e4
Aadil Mugal <aadilshe786@gmail.com>
2023-08-18 22:48:31 +0530

    chore : update template feat-leet-code : add two-sum problem algo

--- leet-code/two-sum.cpp
+++ leet-code/two-sum.cpp
@@ -32,6 +32,23 @@ void solve();
 
 // write your code declaration/defination
 
+vector<int> twoSum(vector<int> &nums, int target) {
+  unordered_map<int, int> umap;
+  vector<int> res;
+
+  for (int i = 0; i < nums.size(); ++i) {
+    int complement = target - nums[i];
+    if (umap.count(complement) > 0) {
+      res.push_back(umap[complement]);
+      res.push_back(i);
+      break;
+    }
+    umap[nums[i]] = i;
+  }
+
+  return res;
+}
+
 //---------------------------------------
 
 int main() {
